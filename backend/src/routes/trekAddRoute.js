73d4b0678a708c775018a60dc1d444a2d3810a04
Prabin17np <prabin17np@gmail.com>
2025-03-01 03:03:02 +0545

    manage all frontend and backend code

--- backend/src/routes/trekAddRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const trekController = require("../controller/trekController");
-const trekAddRouter = express.Router();
-
-trekAddRouter.post("/", trekController.create);
-trekAddRouter.get("/", trekController.getAllTrek);
-
-module.exports = trekAddRouter;
--- backend/src/routes/trekGetRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const trekController = require("../controller/trekController");
-const trekGetRouter = express.Router();
-
-trekGetRouter.get("/", trekController.getAllTrek);
-trekGetRouter.get("/:id", trekController.getTrekById);
-
-module.exports = trekGetRouter;
